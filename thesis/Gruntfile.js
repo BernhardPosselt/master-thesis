33a8b631d5503501f2e15e1fe0973c0f1ff18ede
Bernhard Posselt <dev@bernhard-posselt.com>
2015-02-19 14:24:49 +0100

    add grunt to build when changing

---
+++ thesis/Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    watch: {
+      files: ['*.tex'],
+      tasks: ['shell']
+    },
+    shell: {
+      pdf: {
+        command: 'make'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('default', ['shell']);
+
+};
